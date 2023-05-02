2c0384fe72be7d9177134a87e8ff1cd76e4167f6
unknown <nurubaybaidoi@outlook.com>
2023-05-02 13:59:22 +0700

    first commit

---
+++ page/beranda.php
@@ -0,0 +1,20 @@
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle" id="judul">
+        <img src="asset/gambar/beranda.png"style="width:45px; height:45px;">
+        <div id="judul-text">
+            <h2 class="text-green">BERANDA</h2>
+            Halamanan Utama Administrator
+        </div>
+    </div>
+</div>
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle text-center">
+        <h1>
+            Selamat Datang, <span class="text-green">Administrator</span><br>
+            di Sistem Pendukung Keputusan pemilihan supplier berbasis web menggunakan metode Simple Additive Weighting pada <span class="text-green">Klinik LKC</span>
+        </h1>
+    </div>
+    <div class="panel-bottom"></div>
+</div>
